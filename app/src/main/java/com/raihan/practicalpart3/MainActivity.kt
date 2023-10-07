be65bcdb6950fefdef4d2a28916719c4c749571e
Syed Raihanuzzaman <syed15-10568@diu.edu.bd>
2023-10-07 12:52:55 +0600

    Code is done until toolbar.All is working properly

---
+++ app/src/main/java/com/raihan/practicalpart3/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.raihan.practicalpart3
+import android.app.Person
+import android.os.Build
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.raihan.practicalpart3.R
+import com.raihan.practicalpart3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomnav.setupWithNavController(navController)
+        binding.navview.setupWithNavController(navController)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerlayout, R.string.nav_open, R.string.nav_close)
+        actionBarDrawerToggle.syncState()
+
+        binding.drawerlayout.addDrawerListener(actionBarDrawerToggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navview.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.share ->{
+                    Toast.makeText(this,"You Clicked Share",Toast.LENGTH_SHORT).show()
+                }
+                R.id.Setting ->{
+                    Toast.makeText(this,"You Clicked Setting",Toast.LENGTH_SHORT).show()
+                }
+                R.id.bmiFragment ->{
+                    binding.navview.setupWithNavController(navController)
+                    binding.drawerlayout.closeDrawers()
+                }
+                R.id.tempFragment ->{
+                    binding.navview.setupWithNavController(navController)
+                    binding.drawerlayout.closeDrawers()
+                }
+            }
+            true
+        }
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+        }
+        else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+}
